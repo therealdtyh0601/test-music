5e45d403700133ee4194df540db583a69f28463a
Luminos <doug1992kch@gmail.com>
2025-12-12 00:09:45 +0800

    Create music_pack_B.js

---
+++ assets/js/music/music_pack_B.js
@@ -0,0 +1,20 @@
+export const MusicPack = [
+  {
+    id: "pixel_daybreak",
+    label: "Pixel Daybreak",
+    src: "assets/audio/pixel-daybreak.mp3",
+    loop: true
+  },
+  {
+    id: "pixel_exploration",
+    label: "Pixel Exploration Route",
+    src: "assets/audio/pixel-exploration.mp3",
+    loop: true
+  },
+  {
+    id: "pixel_new_house",
+    label: "Pixel New House Theme",
+    src: "assets/audio/pixel-new-house.mp3",
+    loop: true
+  }
+];
