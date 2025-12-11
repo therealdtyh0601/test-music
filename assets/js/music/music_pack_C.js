26c0754b73146a2c393eb4f726f35c458b1a96a9
Luminos <doug1992kch@gmail.com>
2025-12-12 00:10:15 +0800

    Create music_pack_C.js

---
+++ assets/js/music/music_pack_C.js
@@ -0,0 +1,20 @@
+export const MusicPack = [
+  {
+    id: "ambient_cosmic_air",
+    label: "Ambient Cosmic Air",
+    src: "assets/audio/ambient-cosmic-air.mp3",
+    loop: true
+  },
+  {
+    id: "ambient_home_space",
+    label: "Ambient Home Space",
+    src: "assets/audio/ambient-home-space.mp3",
+    loop: true
+  },
+  {
+    id: "ambient_lightstream",
+    label: "Ambient Lightstream",
+    src: "assets/audio/ambient-lightstream.mp3",
+    loop: true
+  }
+];
